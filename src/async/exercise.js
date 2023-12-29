e6d8fc335c02eb9c7adb5e2512b24cb262bdd950
dr1602 <david.arangelg@gmail.com>
2023-12-28 19:07:28 -0600

    Try & Catch

---
+++ src/async/exercise.js
@@ -0,0 +1,11 @@
+export function runCode() {
+
+    // Tu código aquí 👈
+    const url = 'https://domain-api-com';
+
+    try {
+        throw new Error('API Not found');
+    } catch (error) {
+        throw new Error('API Not Found');
+    }
+}
